8dec5e4a4d4c6aae61c31c9f95ca7456bb349055
MinHieu2811 <cuunhatnhat51@gmail.com>
2023-07-14 23:44:55 +0700

    config auth with Clerk

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
+  publicRuntimeConfig: {
+    clerkSecretKey: process.env.CLERK_SECRET_KEY,
+    clerkPublishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    clerkSignIn: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    clerkSignUp: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    clerkAfterSignIn: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+    clerkAfterSignUp: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
